25714134e36bb7b9193f5d09edfc3572b6a9aedb
Luiz Felipe <lfelipemacedosantos@hotmail.com>
2023-04-19 21:52:08 -0300

    Atividade 9

---
+++ Atividade 9/atividade9.js
@@ -0,0 +1,15 @@
+let nums = [];
+
+for(let i = 0; i<3; i++){
+    nums[i] = prompt("Digite um número");
+    //console.log("numero " + i + " " + nums[i]);
+}
+RetornaMaior(nums);
+OrganizaValores();
+
+function RetornaMaior(a){
+    return alert("O maior número é: " + Math.max(...a));
+}
+function OrganizaValores(a, b){
+    return alert(nums.sort((a, b) => a - b));
+}    
